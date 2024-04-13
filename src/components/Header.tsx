772119201bb7f62a9434cbf55ec6cb8d6a937222
hianshul07 <hianshul07@gmail.com>
2024-04-13 23:51:41 +0530

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import {
+	Navbar,
+	NavbarBrand,
+	NavbarContent,
+	NavbarItem,
+	Input,
+	Button,
+	Avatar,
+} from '@nextui-org/react';
+
+import { auth } from '@/auth';
+
+const Header = async () => {
+	const session = await auth();
+
+	return (
+		<Navbar className='shadow mb-6'>
+			<NavbarBrand className='font-bold'>Discuss</NavbarBrand>
+			<NavbarContent justify='center'>
+				<NavbarItem>
+					<Input />
+				</NavbarItem>
+			</NavbarContent>
+			<NavbarContent justify='end'>
+				<NavbarItem>
+					{session?.user ? <div>Signed In</div> : <div>Signed Out</div>}
+				</NavbarItem>
+			</NavbarContent>
+		</Navbar>
+	);
+};
+export default Header;
